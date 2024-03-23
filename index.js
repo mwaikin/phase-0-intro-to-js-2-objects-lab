c68a9f213873c323c49206b32936fab4cad6a339
mwaikin <mwaikingori10@gmail.com>
2024-03-23 15:25:45 +0300

    done objects labss

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+let employee = {
+    name: "Samson",
+    streetAddress: "woodville close",
+}
+//pdateEmployeeWithKeyAndValu
+    function updateEmployeeWithKeyAndValue(employee,key,value){
+        return {...employee,[key]:value};
+    };
+//destructivelyUpdateEmployeeWithKeyAndValue
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee[key] = value;
+        return employee;
+    }
+
+    //deleteFromEmployeeByKey
+    function deleteFromEmployeeByKey(employee, key){
+        let newEmployee = {...employee};
+        delete newEmployee[key];
+        return newEmployee;
+    }
+    //`destructivelyDeleteFromEmployeeByKe
+    function destructivelyDeleteFromEmployeeByKey(employee, key){
+        let newEmployee = {...employee};
+        delete newEmployee[key];
+        return newEmployee;
+    }
+    function deleteFromEmployeeByKey(employee, key){
+    }
+    
