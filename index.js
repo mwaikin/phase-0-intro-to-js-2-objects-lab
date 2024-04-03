2f438369324db09a7ddb4a7c1ed98959c26cea87
mwaikin <mwaikingori10@gmail.com>
2024-04-03 23:14:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 let employee = {
-    name: "Samson",
-    streetAddress: "woodville close",
+	name: "Joseph Mbugua",
+	streetAddress: 3454566
 }
-//pdateEmployeeWithKeyAndValu
-    function updateEmployeeWithKeyAndValue(employee,key,value){
-        return {...employee,[key]:value};
-    };
-//destructivelyUpdateEmployeeWithKeyAndValue
-    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-        employee[key] = value;
-        return employee;
-    }
 
-    //deleteFromEmployeeByKey
-    function deleteFromEmployeeByKey(employee, key){
-        let newEmployee = {...employee};
-        delete newEmployee[key];
-        return newEmployee;
-    }
-    //`destructivelyDeleteFromEmployeeByKe
-    function destructivelyDeleteFromEmployeeByKey(employee, key){
-        let newEmployee = {...employee};
-        delete newEmployee[key];
-        return newEmployee;
-    }
-    function deleteFromEmployeeByKey(employee, key){
-    }
-    
+//this function updates the object without changing the original object 
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newObj = { ...employee };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(
+  employee,
+  "name",
+  "Sam"
+);
+
+//updates the object but returns a completely new object 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+     employee[key] = value;
+     return employee;
+}
+
+//deleting employee using key without changing original object
+function deleteFromEmployeeByKey(employee,key){
+	//making a clone of the object
+  const newObj = {...employee};
+  delete newObj[key];
+  return newObj;
+
+}
+//deleting employee using key and changing original object 
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+	delete employee[key];
+	return employee;
+}
